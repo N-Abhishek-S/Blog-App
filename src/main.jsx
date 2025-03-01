0ea5025406151bae14449c35914ce40fb6f429e4
N-Abhishek-S <nagargojeabhishek96@gmail.com>
2025-03-01 21:14:34 +0530

    Deployment Problem solve

--- src/main.jsx
+++ src/main.jsx
@@ -15,66 +15,67 @@ import {
   AllPost,
 } from "./componets/index.js";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/login",
+          element: (
+            <AuthLayout authentication={false}>
+              <Login />
+            </AuthLayout>
+          ),
+        },
+        {
+          path: "/signup",
+          element: (
+            <AuthLayout authentication={false}>
+              <Signup />
+            </AuthLayout>
+          ),
+        },
+        {
+          path: "/all-posts",
+          element: (
+            <AuthLayout authentication>
+              <AllPost />
+            </AuthLayout>
+          ),
+        },
+        {
+          path: "/add-post",
+          element: (
+            <AuthLayout authentication>
+              <AddPost />
+            </AuthLayout>
+          ),
+        },
+        {
+          path: "/edit-post/:slug",
+          element: (
+            <AuthLayout authentication>
+              <EditPost />
+            </AuthLayout>
+          ),
+        },
+        {
+          path: "/post/:slug",
+          element: <Post />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/login",
-        element: (
-          <AuthLayout authentication={false}>
-            <Login />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/signup",
-        element: (
-          <AuthLayout authentication={false}>
-            <Signup />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/all-posts",
-        //Empty authentication means need anuthentication = true;
-        element: (
-          <AuthLayout authentication>
-            {" "}
-            <AllPost />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/add-post",
-        element: (
-          <AuthLayout authentication>
-            {" "}
-            <AddPost />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/edit-post/:slug",
-        element: (
-          <AuthLayout authentication>
-            {" "}
-            <EditPost />
-          </AuthLayout>
-        ),
-      },
-      {
-        path: "/post/:slug",
-        element: <Post />,
-      },
-    ],
-  },
-]);
+    basename: "/Blog-App", // <-- This is the important part
+  }
+);
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <RouterProvider router={router} />
