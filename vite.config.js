a5f5e300ede0074cd29dc83ef8be93a54a21bc13
N-Abhishek-S <nagargojeabhishek96@gmail.com>
2025-03-03 16:54:39 +0530

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/BlogApp/',  // 👈 This must match your repo name exactly
-})
+  base: '/Blog-App/' // ⚠️ This must match your repo name exactly (case-sensitive)
+});
